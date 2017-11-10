f567cc19943fae012aecca6309ead15726fa0f6a
pr0f33 <stephanework@hotmail.com>
2017-11-10 13:57:03 -0400

    f

--- resources/[police]/bison/__resource.lua
+++ resources/[police]/bison/__resource.lua
@@ -1,8 +1,10 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
  
 files {
-	'carcols.meta'
+	'carcols.meta',
+	'carvariations.meta'
 }
  
 data_file 'CARCOLS_META' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
  
